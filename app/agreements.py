6bf9fdc98953f37303ee5f090ca510383cee356a
Mikkel Præstegaard <mikkel.fiil.praestegaard@gmail.com>
2026-05-31 19:12:54 +0200

    Finished agreements page

--- app/agreements.py
+++ app/agreements.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-
+import ast
 from werkzeug.exceptions import abort
 from app.init_db import get_db
 import psycopg2
@@ -21,6 +21,18 @@ def table():
 
     return render_template('agreements.html', agreements=agreements)
     
+
+def parse_value(value):
+    if isinstance(value, str):
+        try:
+            parsed = ast.literal_eval(value)
+            if isinstance(parsed, (list, dict)):
+                return parsed
+        except (ValueError, SyntaxError):
+            pass
+    return value
+
+
 @bp.route('/agreements/<int:id>')
 def agreement(id):
     db = get_db().cursor(cursor_factory=psycopg2.extras.RealDictCursor)
@@ -30,6 +42,6 @@ def agreement(id):
                 WHERE parsed_agreement_text."Agreement_ID" = %s
                ''', (id,))
     query_result = db.fetchone()
-    agreement_data = dict(query_result)
+    agreement_data = {k: parse_value(v) for k, v in dict(query_result).items()}
 
     return render_template('agreements_text.html', agreement_data=agreement_data)
